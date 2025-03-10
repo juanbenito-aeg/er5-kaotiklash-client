5eecdb9fdb7e16d4e5a994196494ea6397c74b31
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-10 19:14:54 +0100

    feat(kk-31): display game board after clicking start game button

---
+++ src/index.js
@@ -0,0 +1,20 @@
+window.onload = init;
+
+
+function showImage() {
+    
+    const startGameScreen = document.getElementById("start-game-screen");
+    const gameScreen = document.getElementById("game-screen");
+   
+    startGameScreen.style.display = "none";
+    gameScreen.style.display = "block";
+
+}
+
+function init() {
+  
+    const btn = document.getElementById("start-game-btn");
+
+    btn.addEventListener("click", showImage); 
+
+}
