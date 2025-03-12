c11f600372bbbb63ef3b1ee107f3051db4bb2841
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-03-13 00:07:10 +0100

    refactor(kk-43): replace div as container of game screen with canvas element

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import Game from "./Game/Game.js";
 
-// INITIALIZATION OF GLOBAL VARIABLES
+// GLOBAL VARIABLES INITIALIZATION
 const globals = {
-    game: {},
+  canvas: {},
+  ctx: {},
+  game: {},
 };
 
-function initGame() {
-    const startGameScreen = document.getElementById("start-game-screen");
-    startGameScreen.style.display = "none";
-    
-    const gameScreen = document.getElementById("game-screen");
-    gameScreen.style.display = "block";
-    
-    globals.game = Game.create();
+function initGameScreen() {
+  const startGameScreen = document.getElementById("start-game-screen");
+  startGameScreen.style.display = "none";
+
+  globals.canvas = document.getElementById("game-screen");
+  globals.canvas.style.display = "block";
+
+  globals.ctx = globals.canvas.getContext("2d");
+
+  globals.game = Game.create();
 }
 
-window.onload = initEssentials;
+window.onload = initStartGameScreen;
 
-function initEssentials() {
-    const btn = document.getElementById("start-game-btn");
-    btn.addEventListener("click", initGame);
+function initStartGameScreen() {
+  const btn = document.getElementById("start-game-btn");
+  btn.addEventListener("click", initGameScreen);
 }
 
-export {
-    globals,
-};
+export { globals };
