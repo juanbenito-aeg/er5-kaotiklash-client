b6d62f4d6ad78a42a1b4f116c82d2a1cf404059c
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-13 01:48:04 +0100

    refactor(kk-43): implement deckcreator class to create and distribute decks for players based on card categories

--- src/Decks/DeckCreator.js
+++ src/Decks/DeckCreator.js
@@ -1,5 +1,6 @@
 import CardFactory from "./CardFactory.js";
 import DeckContainer from "./DeckContainer.js";
+import { Category } from "../Game/constants.js";
 
 export default class DeckCreator {
   #cardsData;
@@ -28,18 +29,54 @@ export default class DeckCreator {
       handDeck: [],
       minionsInPlayDeck: [],
       minionsDeck: [],
-      eventPreparationDeck: [],
       mainCharacterDeck: [],
+      preparationEventsDeck: []
     };
 
     const player2Decks = {
       handDeck: [],
       minionsInPlayDeck: [],
       minionsDeck: [],
-      eventPreparationDeck: [],
       mainCharacterDeck: [],
+      preparationEventsDeck: []
     };
 
     const eventsDeck = [];
+
+    for (let i = 0; i < mainDeck.cardFactory.length; i++){
+        const card = mainDeck.cardFactory[i];
+        
+        switch(card.Category){
+            case Category.MAIN_CHARACTERS:
+                    player1Decks.mainCharacterDeck.push(card);
+                    player2Decks.mainCharacterDeck.push(card);
+                break;
+            case Category.MINIONS:
+                    player1Decks.minionsDeck.push(card);
+                    player2Decks.minionsDeck.push(card);
+                break;
+            case Category.EVENTS:
+                eventsDeck.push(card);
+                break;
+            case Category.PREPARATION_EVENTS:
+                player1Decks.preparationEventsDeck.push(card);
+                player2Decks.preparationEventsDeck.push(card);
+                break;
+            default:
+                    player1Decks.handDeck.push(card);
+                    player2Decks.handDeck.push(card);
+                    break;
+        }
+    }
+
+    const allDecks = [
+        eventsDeck, player1Decks.preparationEventsDeck, player1Decks.handDeck, 
+        player1Decks.mainCharacterDeck, player1Decks.minionsDeck, 
+        player1Decks.minionsInPlayDeck, player2Decks.preparationEventsDeck,
+        player2Decks.handDeck, player2Decks.mainCharacterDeck, 
+        player2Decks.minionsDeck, player2Decks.minionsInPlayDeck
+    ];
+     
+    return new DeckContainer(allDecks);
   }
 }
