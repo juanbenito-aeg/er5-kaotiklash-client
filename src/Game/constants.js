56f5a5bf926966999f509431c83a2dae14172541
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-03-13 21:19:49 +0100

    feat(kk-43): create objects to store constant values that identify the main characters, Joseph's chaotic events & the game language

--- src/Game/constants.js
+++ src/Game/constants.js
@@ -1,6 +1,28 @@
-export const Category = {
-    MAIN_CHARACTERS:      0,
-    MINIONS:              1,
-    EVENTS:               2,
-    PREPARATION_EVENTS:   3
-};  
+const Language = {
+  ENGLISH: 0,
+  BASQUE: 1,
+};
+
+const Category = {
+  MAIN_CHARACTERS: 0,
+  MINIONS: 1,
+  EVENTS: 2,
+  PREPARATION_EVENTS: 3,
+};
+
+const MainCharacterID = {
+  LUCRETIA: 0,
+  ANGELO_DI_MORTIS: 1,
+  THE_ERUDITE_XG: 2,
+  THE_DECREPIT_THRONE: 3,
+  JOSEPH: 4,
+};
+
+const ChaoticEventID = {
+  LIGHTNING_STORM: 0,
+  SANDSTORM: 1,
+  MINION_REBELLION: 2,
+  CONSTANT_SWAP: 3,
+};
+
+export { Language, Category, MainCharacterID, ChaoticEventID };
