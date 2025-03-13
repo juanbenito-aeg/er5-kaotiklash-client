e2159f7090e3cc62a3f9c5d613062f896c5086e4
Rosalia <rosalia.campusano@ikasle.aeg.eus>
2025-03-13 14:00:40 +0100

    refactor(kk-43): remove presumably innecessary attribute of the Card class
    Changes made collaboratively making use of Live Share. Session
    participants: Juan, Rosalía.

--- src/Decks/Card.js
+++ src/Decks/Card.js
@@ -1,14 +1,12 @@
 export default class Card {
   #id;
-  #category;
   #nameENG;
   #nameEUS;
   #descriptionENG;
   #descriptionEUS;
 
-  constructor(id, category, nameENG, nameEUS, descriptionENG, descriptionEUS) {
+  constructor(id, nameENG, nameEUS, descriptionENG, descriptionEUS) {
     this.#id = id;
-    this.#category = category;
     this.#nameENG = nameENG;
     this.#nameEUS = nameEUS;
     this.#descriptionENG = descriptionENG;
