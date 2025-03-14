2b8ef81dcd8e958496ad1fffbf946f1f51867db4
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-14 01:25:26 +0100

    feat(kk-43): add PerformEventPhase class for event handling logic (incomplete)

---
+++ src/Turns/PerformEventPhase.js
@@ -0,0 +1,18 @@
+import Phase from "./Phase.js";
+
+export default class PerformEventPhase extends Phase{
+    #eventsPreparationDeck;
+    #eventsPreparationGrid;
+    #activeEventsDeck;
+    #currentPlayerMovementGridDeck;
+    #enemyMovementGridDeck;
+    #state;
+
+    constructor(eventsPreparationDeck, eventsPreparationGrid, activeEventsDeck, currentPlayerMovementGridDeck, enemyMovementGridDeck){
+        this.#eventsPreparationDeck = eventsPreparationDeck;
+        this.#eventsPreparationGrid = eventsPreparationGrid;
+        this.#activeEventsDeck = activeEventsDeck;
+        this.#currentPlayerMovementGridDeck = currentPlayerMovementGridDeck;
+        this.#enemyMovementGridDeck = enemyMovementGridDeck;
+    }
+}
