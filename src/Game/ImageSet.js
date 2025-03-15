4ed2d456f1f9de53a48b8537ed611dfe53cf3b6f
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-03-15 23:26:46 +0100

    feat(kk-41): create getter & setter methods on ImageSet class

--- src/Game/ImageSet.js
+++ src/Game/ImageSet.js
@@ -7,21 +7,44 @@ export default class ImageSet {
   #xDestinationSize;
   #yDestinationSize;
 
-  constructor(
-    image,
-    xInit,
-    yInit,
-    xSize,
-    ySize,
-    xDestinationSize,
-    yDestinationSize
-  ) {
-    this.#image = image;
+  constructor(xInit, yInit, xSize, ySize, xDestinationSize, yDestinationSize) {
     this.#xInit = xInit;
     this.#yInit = yInit;
     this.#xSize = xSize;
     this.#ySize = ySize;
     this.#xDestinationSize = xDestinationSize;
     this.#yDestinationSize = yDestinationSize;
+  }
+
+  getImage() {
+    return this.#image;
+  }
+
+  getXInit() {
+    return this.#xInit;
+  }
+
+  getYInit() {
+    return this.#yInit;
+  }
+
+  getXSize() {
+    return this.#xSize;
+  }
+
+  getYSize() {
+    return this.#ySize;
+  }
+
+  getXDestinationSize() {
+    return this.#xDestinationSize;
+  }
+
+  getYDestinationSize() {
+    return this.#yDestinationSize;
+  }
+
+  setImage(newImage) {
+    this.#image = newImage;
   }
 }
