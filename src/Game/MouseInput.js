97c9120919edd9bd0384510f7360406a129567dd
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-03-18 00:23:47 +0100

    feat(kk-45): tweak some event listeners so that they function correctly & add getter methods on MouseInput
    Changes made collaboratively making use of Live Share. Session
    participants: Juan, Rosalía.

--- src/Game/MouseInput.js
+++ src/Game/MouseInput.js
@@ -1,41 +1,78 @@
 import { globals } from "../index.js";
-export default class MouseInput {
 
+export default class MouseInput {
   #mouseXCoordinate;
   #mouseYCoordinate;
   #leftButtonPressed;
-  #rigthButtonPressed;
+  #rightButtonPressed;
 
-  constructor(mouseXCoordinate, mouseYCoordinate, leftButtonPressed, rigthButtonPressed) {
-    this.#mouseXCoordinate = mouseXCoordinate;
-    this.#mouseYCoordinate = mouseYCoordinate;
-    this.#leftButtonPressed = leftButtonPressed;
-    this.#rigthButtonPressed = rigthButtonPressed;
+  constructor() {
+    this.#mouseXCoordinate = 0;
+    this.#mouseYCoordinate = 0;
+    this.#leftButtonPressed = false;
+    this.#rightButtonPressed = false;
   }
 
-  mouseEventListener() {
+  addMouseEventListeners() {
     window.addEventListener("contextmenu", function (event) {
       // DISABLE THE CONTEXT-MENU FOR THE WHOLE PAGE
       event.preventDefault();
     });
-    globals.canvas.addEventListener("mouseup", this.canvasMouseHandler.bind(this), false);
-    globals.canvas.addEventListener("mousedown", this.canvasMouseDownHandler.bind(this), false);
-    globals.canvas.addEventListener("mousemove", this.canvasMouseMoveHandler.bind(this), false);
+
+    globals.canvas.addEventListener(
+      "mousedown",
+      this.canvasMouseDownHandler.bind(this),
+      false
+    );
+
+    globals.canvas.addEventListener(
+      "mouseup",
+      this.canvasMouseUpHandler.bind(this),
+      false
+    );
+
+    globals.canvas.addEventListener(
+      "mousemove",
+      this.canvasMouseMoveHandler.bind(this),
+      false
+    );
   }
 
-  canvasMouseHandler() {
-    this.#leftButtonPressed = true;
-    this.#rigthButtonPressed = true;
+  canvasMouseDownHandler(event) {
+    if (event.button === 0) {
+      this.#leftButtonPressed = true;
+    } else if (event.button === 2) {
+      this.#rightButtonPressed = true;
+    }
   }
 
-  canvasMouseDownHandler() {
-    this.#leftButtonPressed = false;
-    this.#rigthButtonPressed = false;
+  canvasMouseUpHandler(event) {
+    if (event.button === 0) {
+      this.#leftButtonPressed = false;
+    } else if (event.button === 2) {
+      this.#rightButtonPressed = false;
+    }
   }
 
   canvasMouseMoveHandler(event) {
     //FIND THE MOUSE'S X AND Y POSITIONS ON THE CANVAS
     this.#mouseXCoordinate = event.pageX - globals.canvas.offsetLeft;
     this.#mouseYCoordinate = event.pageY - globals.canvas.offsetTop;
+  }
+
+  getMouseXCoordinate() {
+    return this.#mouseXCoordinate;
+  }
+
+  getMouseYCoordinate() {
+    return this.#mouseYCoordinate;
+  }
+
+  isLeftButtonPressed() {
+    return this.#leftButtonPressed;
+  }
+
+  isRightButtonPressed() {
+    return this.#rightButtonPressed;
   }
 }
