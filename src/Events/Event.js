cff19929e6598062523c82ed49d868e96e8bbfc5
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-07 16:03:26 +0200

    feat(kk-100): create the a method that reduce the duration & is Active method to know when to splice the event
    Changes made collaboratively making use of Live Share. Session
    participants: Juan, Rosalía.

--- src/Events/Event.js
+++ src/Events/Event.js
@@ -1,5 +1,34 @@
 export default class Event {
+  _executedBy;
+  _lastPlayer;
+  _eventCard;
+
+  constructor(executedBy, eventCard) {
+    this._executedBy = this._lastPlayer = executedBy;
+    this._eventCard = eventCard;
+  }
+
   static create() {}
 
   execute() {}
+
+  reduceDuration(currentPlayer) {
+    if (currentPlayer !== this._lastPlayer) {
+      this._lastPlayer = currentPlayer;
+
+      if (currentPlayer === this._executedBy) {
+        this._eventCard.setCurrentDurationInRounds(
+          this._eventCard.getCurrentDurationInRounds() - 1
+        );
+      }
+    }
+  }
+
+  isActive() {
+    if (this._eventCard.getCurrentDurationInRounds() > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
