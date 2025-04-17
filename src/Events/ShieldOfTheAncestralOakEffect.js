2de1c47bb0ef65e7e88fbfa65401922e5fcc5184
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-18 01:35:18 +0200

    feat(kk-143): create the special effect of the heavy Armor

---
+++ src/Events/ShieldOfTheAncestralOakEffect.js
@@ -0,0 +1,30 @@
+import StateMessage from "../Messages/StateMessage.js";
+
+export default class ShieldOfYheAncestralOakEffect {
+  static applyCounterAttack(target, stateMessages) {
+    const counterDamage = 20;
+
+    let targetHP = target.getCurrentHP() - counterDamage;
+    if (targetHP < 0) targetHP = 0;
+    target.setCurrentHP(targetHP);
+
+    const message = new StateMessage(
+      "COUNTERATTACK: SHIELD OF THE ANCESTRAL OAK!",
+      "40px MedievalSharp",
+      "green",
+      2,
+      1200,
+      520
+    );
+    const damageMessage = new StateMessage(
+      `-${counterDamage} HP`,
+      "30px MedievalSharp",
+      "darkgreen",
+      2,
+      1200,
+      570
+    );
+
+    stateMessages.push(message, damageMessage);
+  }
+}
