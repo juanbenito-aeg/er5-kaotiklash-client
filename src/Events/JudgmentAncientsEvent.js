cbc8ca76f1730a68843eee39c85a44d102060074
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-28 22:44:26 +0200

    refactor(kk-129): insert event data into CurseOfTheBoundTitan, JudgmentAncientsEvent, and PoisonOfTheAbyssEvent

--- src/Events/JudgmentAncientsEvent.js
+++ src/Events/JudgmentAncientsEvent.js
@@ -1,27 +1,28 @@
 import Event from "./Event.js";
-import globals from "../Game/globals.js";
 import { PlayerID } from "../Game/constants.js";
 
 export default class JudgmentAncientsEvent extends Event {
-  constructor(executedBy, eventCard) {
+  #eventsData;
+  constructor(executedBy, eventCard, eventsData) {
     super(executedBy, eventCard);
+    this.#eventsData = eventsData;
   }
 
   execute(currentPlayer) {
     this.reduceDuration(currentPlayer);
 
-    globals.judgmentAncientsEventData.isActive = true;
+    this.#eventsData.judgmentAncients.isActive = true;
 
     if (this._executedBy.getID() === PlayerID.PLAYER_1) {
-      globals.judgmentAncientsEventData.affectedPlayerID = PlayerID.PLAYER_2;
+      this.#eventsData.judgmentAncients.affectedPlayerID = PlayerID.PLAYER_2;
     } else {
-      globals.judgmentAncientsEventData.affectedPlayerID = PlayerID.PLAYER_1;
+      this.#eventsData.judgmentAncients.affectedPlayerID = PlayerID.PLAYER_1;
     }
 
     if (!this.isActive()) {
-      globals.judgmentAncientsEventData.isActive = false;
+      this.#eventsData.judgmentAncients.isActive = false;
 
-      globals.judgmentAncientsEventData.affectedPlayerID = -1;
+      this.#eventsData.judgmentAncients.affectedPlayerID = -1;
     }
   }
 }
