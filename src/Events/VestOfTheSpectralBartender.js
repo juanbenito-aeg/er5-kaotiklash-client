8b1bafa91242b5481209bfbe45adc026815dcc66
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-05-03 20:00:31 +0200

    refactor(kk-181): tweak the state message specific to the effect of the "Vest of the Spectral Bartender"

--- src/Events/VestOfTheSpectralBartender.js
+++ src/Events/VestOfTheSpectralBartender.js
@@ -2,9 +2,8 @@ import StateMessage from "../Messages/StateMessage.js";
 
 export default class VestOfTheSpectralBartenderEffect {
   static blockCrit(damage, target, stateMessage) {
-
     const msg = new StateMessage(
-      "CRITICAL HIT BLOCKED!",
+      "EXTRA DAMAGE OF THE CRITICAL HIT IGNORED!",
       "30px MedievalSharp",
       "orange",
       2,
