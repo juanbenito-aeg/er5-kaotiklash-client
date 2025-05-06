64cf3de1df4391d2f9d54e0702ae5fca498b7474
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-05-06 23:45:24 +0200

    feat(kk-211): create the PlayerStats class, which will serve as a container for player-specific statistics, a function that is currently incorrectly performed by the Player class

---
+++ src/Game/PlayerStats.js
@@ -0,0 +1,13 @@
+export default class PlayerStats {
+  #minionsKilled;
+  #fumbles;
+  #criticalHits;
+  #usedCards;
+
+  constructor() {
+    this.#minionsKilled = 0;
+    this.#fumbles = 0;
+    this.#criticalHits = 0;
+    this.#usedCards = 3;
+  }
+}
