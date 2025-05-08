69327432223270832fe96374fb48de214bc0a7ef
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-05-08 10:15:14 +0200

    refactor(kk-211): remove unused getter methods for statistics from the Player class

--- src/Game/Player.js
+++ src/Game/Player.js
@@ -2,18 +2,10 @@ export default class Player {
   #id;
   #name;
   #totalHP;
-  #minions_killed;
-  #fumbles;
-  #critical_hits;
-  #used_cards;
 
   constructor(id, name) {
     this.#id = id;
     this.#name = name;
-    this.#minions_killed = 0;
-    this.#fumbles = 0;
-    this.#critical_hits = 0;
-    this.#used_cards = 3;
   }
 
   getID() {
@@ -34,21 +26,5 @@ export default class Player {
 
   setTotalHP(newTotalHP) {
     this.#totalHP = newTotalHP;
-  }
-
-  getMinionsKilled() {
-    return this.#minions_killed;
-  }
-
-  getFumbles() {
-    return this.#fumbles;
-  }
-
-  getCriticalHits() {
-    return this.#critical_hits;
-  }
-
-  getUsedCards() {
-    return this.#used_cards;
   }
 }
