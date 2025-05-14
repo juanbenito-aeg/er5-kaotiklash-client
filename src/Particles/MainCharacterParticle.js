c594a0c0d59e0102eab8801a3765f4ca06607732
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-05-14 10:43:27 +0200

    refactor(kk-194): add pulsating effect to main character particles and improve appearance

--- src/Particles/MainCharacterParticle.js
+++ src/Particles/MainCharacterParticle.js
@@ -8,6 +8,8 @@ export default class MainCharacterParticle extends Particle {
   #fadeCounter;
   #currentPlayer;
   #radius;
+  #baseSize;
+
   constructor(
     id,
     state,
@@ -24,6 +26,7 @@ export default class MainCharacterParticle extends Particle {
     this.#fadeCounter = 0;
     this.#radius = radius;
     this.#currentPlayer = currentPlayer;
+    this.#baseSize = 3 + Math.random();
   }
 
   static create(particles, num, box, currentPlayer) {
@@ -38,8 +41,7 @@ export default class MainCharacterParticle extends Particle {
       const x = centerX + distanceFromCenter * Math.cos(angle);
       const y = centerY + distanceFromCenter * Math.sin(angle);
 
-      const velocity = Math.random() * 15 + 5;
-      const physics = new Physics(velocity, 0);
+      const physics = new Physics(0, 0);
 
       const particle = new MainCharacterParticle(
         ParticleID.MAIN_CHARACTER,
@@ -77,24 +79,30 @@ export default class MainCharacterParticle extends Particle {
         this._state = ParticleState.OFF;
       }
     }
-
-    const speed = 2;
-    this.#angle += 0.05 * globals.deltaTime;
-    this._xCoordinate += Math.cos(this.#angle) * speed * globals.deltaTime;
-    this._yCoordinate += Math.sin(this.#angle) * speed * globals.deltaTime;
   }
 
   render() {
     globals.ctx.save();
     globals.ctx.globalAlpha = this._alpha;
 
-    if (this._state === ParticleState.SPAWN) {
-      globals.ctx.fillStyle = `rgba(255, 255, 255, 0.9)`;
-    } else {
-      globals.ctx.fillStyle = `rgba(255, 165, 0, 0.8)`;
-    }
+    const scale = 1 + 0.3 * Math.sin(this.#fadeCounter * 3);
+    const radius = this.#baseSize * scale;
+
+    const hue = (this.#fadeCounter * 60) % 360;
+    const color =
+      this._state === ParticleState.SPAWN
+        ? `hsl(${hue}, 100%, 85%)`
+        : `hsl(${hue}, 100%, 60%)`;
+
+    globals.ctx.fillStyle = color;
     globals.ctx.beginPath();
-    globals.ctx.arc(this._xCoordinate, this._yCoordinate, 3, 0, 2 * Math.PI);
+    globals.ctx.arc(
+      this._xCoordinate,
+      this._yCoordinate,
+      radius,
+      0,
+      2 * Math.PI
+    );
     globals.ctx.fill();
 
     globals.ctx.restore();
